8486891c2da04cd03d4caeb4c8c9e92667ebf97d
Adam Trincas <atrincas@gmail.com>
2019-03-19 13:50:34 +0100

    import header.js statement fixed

--- src/components/searchForm/Confirmation.js
+++ src/components/searchForm/Confirmation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Header from '../Header';
+import Header from './Header';
 
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
