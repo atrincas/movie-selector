f04fdcffabd8b30ac0eb222dc990276d3d76c66f
Adam Trincas <atrincas@gmail.com>
2019-03-25 19:31:44 +0100

    m~ovie.title added inside carousel

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -49,7 +49,7 @@ const Carousel = (props) => {
       {props.popularMovies.map(movie => (
       	<div key={movie.id} className="movie-card">
 	      	<img alt="movie" className="swiper-slide__image" src="https://image.tmdb.org/t/p/w154/xvx4Yhf0DVH8G4LzNISpMfFBDy2.jpg" />
-	      	<h3 className="swiper-slide__title">Movie Title</h3>
+	      	<h3 className="swiper-slide__title">{movie.title}</h3>
 	      	<p className="swiper-slide-rating">
 	        <svg className="swiper-slide-rating__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
 	        <path d="M256 372.686L380.83 448l-33.021-142.066L458 210.409l-145.267-12.475L256 64l-56.743 133.934L54 210.409l110.192 95.525L131.161 448z"/></svg>
